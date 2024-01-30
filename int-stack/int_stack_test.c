49139603676c3251faeb477a986b761cfb6f3080
Va1p3n <88006936+Va1p3n@users.noreply.github.com>
2024-01-30 15:53:49 +0100

    added function-comments

--- int-stack/int_stack_test.c
+++ int-stack/int_stack_test.c
@@ -15,156 +15,235 @@
 */
 
 /**
- * Compares two values stored in the stack
+ * Compares two values stored in the stack.
+ * 
+ * Returns: True if the values are the same, otherwise false.
 */
 bool value_equal(int v1, int v2) {
 	return v1 == v2;
 }
 
+/**
+ * empty_is_empty_test() - Test that stack_empty is empty.
+ * 
+ * Returns: Nothing.
+*/
 void empty_is_empty_test() {
 	fprintf(stderr, "Starting empty_is_empty_test()...");
 
+	// Creates empty stack.
 	stack s = stack_empty();
 
+	// Stack should be empty.
 	if (!stack_is_empty(s))
 	{
+		// Fails with error message
 		fprintf(stderr, "FAIL: Expected is_empty(empty()) == true, got false\n");
 		exit(EXIT_FAILURE);
 	}
 
+	// Everything went as planned, clean up
 	fprintf(stderr, "Cleaning up..");
 	stack_kill(s);
 	fprintf(stderr, "DONE!\n");
 }
 
+/**
+ * one_element_in_stack_is_nonempty() - Test that is_empty returns false if the stack has items.
+ * 
+ * Returns: Nothing.
+*/
 void one_element_in_stack_is_nonempty() {
 	fprintf(stderr, "Starting one_element_in_nonempty()...");
 
+	// Creates empty stack and pushes a value to it
 	stack s = stack_empty();
 	s = stack_push(s, 5);
 
+	// stack_is_empty() should return false
 	if (stack_is_empty(s))
 	{
+		// Fail with error message
 		fprintf(stderr, "FAIL: Expected is_empty() == false, got true\n");
 		stack_kill(s);
 		exit(EXIT_FAILURE);
 	}
 	
-
+	// Everything went as planned, clean up
 	fprintf(stderr, "Cleaning up...");
 	stack_kill(s);
 	fprintf(stderr, "DONE!\n");
 }
 
+/**
+ * inserted_element_has_the_right_value() - Tests that inserted element gets inserted to the stack
+ * 		with the correct value.
+ * 
+ * Returns: Nothing
+*/
 void inserted_element_has_the_right_value() {
 	fprintf(stderr, "Starting inserted_element_has_the_right_value()...");
 
+	// Creates a stack and adds a specific to the stack
 	int val = 13;
 	stack s = stack_empty();
 	s = stack_push(s, val);
 
+	// Reads the top value of the stack 
 	int read_value = stack_top(s);
 
+	// read_value should be the same as val
 	if (read_value != val)
 	{
+		// Fails with error message
 		fprintf(stderr, "FAIL: Expected top() == %d, got %d\n", val, read_value);
 		stack_kill(s);
 		exit(EXIT_FAILURE);
 	}
 	
+	// Everything went as planned, clean up
 	fprintf(stderr, "Cleaning up...");
 	stack_kill(s);
 	fprintf(stderr, "DONE!\n");
 }
 
+/**
+ * read_top_test() - Tests that stack_top() reads the correct element.
+ * 
+ * Returns: Nothing.
+*/
 void read_top_test() {
 	fprintf(stderr, "Starting read_top_test()...");
 	
+	// Creates empty stack and pushes two valus to it
 	int val = 74;
 	int val2 = 47;
 	stack s = stack_empty();
 	s = stack_push(s, val);
 	s = stack_push(s, val2);
 
+	// Expects value_equal to be false.
 	if (value_equal(stack_top(s), val))
 	{
-		fprintf(stderr, "FAIL: Expected top() to return element at top but bottom element");
+		// Fails with error message
+		fprintf(stderr, "FAIL: Expected top() to return element at top but bottom element\n");
 		stack_kill(s);
 		exit(EXIT_FAILURE);
 	}
 
+	// Everything went as planned, clean up
 	fprintf(stderr, "Cleaning up...");
 	stack_kill(s);
 	fprintf(stderr, "DONE!\n");
 }
 
+/**
+ * top_is_constant() - Tests wether the value that top() reads is constant.
+ * 
+ * Returns: Nothing
+*/
 void top_is_constant() {
 	fprintf(stderr, "Starting top_is_constant()...");
 
+	// Creates empty stack, array of values to write and an empty array to fill with read values.
 	int values[] = {1, 2};
 	int read_values[2];
 	stack s = stack_empty();
 
+	// Pushes the values to the stack and reads top().
 	for (int i = 0; i < 2; i++)
 	{
 		s = stack_push(s, values[i]);
-		values[i] = stack_top(s);
+		read_values[i] = stack_top(s);
 	}
 
-	if (value_equal(read_values[0], read_values[2]))
+	// value_equal should be false here
+	if (value_equal(read_values[0], read_values[1]))
 	{
-		fprintf(stderr, "FAIL: top() returns constant value.");
+		// Fails with error message
+		fprintf(stderr, "FAIL: top() returns constant value.\n");
 		stack_kill(s);
 		exit(EXIT_FAILURE);
 	}
 	
+	// Everything went as planned, clean up
 	fprintf(stderr, "Cleaning up...");
 	stack_kill(s);
 	fprintf(stderr, "DONE!\n");
 }
 
+/**
+ * inserted_elements_at_correct_place() - Tests wether stack_push() adds elements att wrong places.
+ * 
+ * Returns: Nothing.
+*/
 void inserted_elements_at_correct_place() {
 	fprintf(stderr, "Starting inserted_elements_at_correct_place()...");
 
+	// Creates empty stack and an array of values to push to the stack
 	int n = 5;
 	int values[] = {321, 21, 42, 6, 13};
 	stack s = stack_empty();
 
-	// Writes values to stack
+	// Pushes values to stack
 	for (int i = 0; i < n; i++)
 	{
 		s = stack_push(s, values[i]);
 	}
 	
-	// read values from stack and compares them
+	// Loops through the stack
 	for (int i = 0; i < n; i++)
 	{
+		// read values from stack and compares them
 		if (!value_equal(values[(n - 1) - i], stack_top(s)))
 		{
-			fprintf(stderr, "FAIL: Expected top() element nr.%d to be %d, got %d", i+1, values[i], stack_top(s));
+			// Fail with error message
+			fprintf(stderr, "FAIL: Expected top() element nr.%d to be %d, got %d\n", i+1, values[i], stack_top(s));
 			stack_kill(s);
 			exit(EXIT_FAILURE);
 		}
+
+		// Removes top element from stack.
 		s = stack_pop(s);
 	}
 
+	// Everything went as planned
 	fprintf(stderr, "Cleaning up...");
 	stack_kill(s);
 	fprintf(stderr, "DONE!\n");
 }
 
+/**
+ * inserted_too_many_values() - Test if stack_push adds to many values to the stack.
+ * 
+ * Returns: nothing
+*/
 void inserted_too_many_values() {
 	fprintf(stderr, "Starting inserted_too_many_values()...");
 	
+	// Creates empty stack and pushes a value to it and removes that value
 	int val = 74;
 	stack s = stack_empty();
-	s = stack_push(s, val);
+	s = stack_pop(stack_push(s, val));
 
+	// stack_is_empty() should return true
+	if (!stack_is_empty(s))
+	{
+		// Fail with error message
+		fprintf(stderr, "FAIL: Expected stack_is_empty() == true, got false. Too many elements added.\n");
+		stack_kill(s);
+		exit(EXIT_FAILURE);
+	}
+	
+	// Everything went as planned, clean up.
 	fprintf(stderr, "Cleaning up...");
 	stack_kill(s);
 	fprintf(stderr, "DONE!\n");
 }
 
+/**
+ * main() - Runs all the tests and outputs it all tests passed.
+*/
 int main(void)
 {
 	empty_is_empty_test();
