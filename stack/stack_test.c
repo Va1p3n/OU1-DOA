0a07545e50dea715549a82ea1ea21fffc5c6be12
Va1p3n <88006936+Va1p3n@users.noreply.github.com>
2024-01-30 17:58:38 +0100

    start stack I think ill be done with int_stack for now

---
+++ stack/stack_test.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+#include <stack.h>
+
+/**
+ * Test program to test implementations of the datatype stack
+ * 
+ * Author: Felix Vallström (c23fvm)
+ * 
+ * Hand in date: 
+ * 
+ * Version:
+ *   2024-01-XX: v1.0. First hand in
+*/
