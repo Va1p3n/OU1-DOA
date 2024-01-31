549a60c429b35f17d47d5a69be4dcfe38a5e815c
Va1p3n <88006936+Va1p3n@users.noreply.github.com>
2024-01-31 16:48:26 +0100

    yes, progress i think

--- stack/stack_test.c
+++ stack/stack_test.c
@@ -14,6 +14,10 @@
  *   2024-01-XX: v1.0. First hand in
 */
 
+void print_f(const void* i) {
+	printf("%d", *((int*)i));
+}
+
 /**
  * value_equal() - Compares two values stored in the stack.
  * @v1: value (integer) 1
@@ -37,8 +41,22 @@ int *create_element(int i) {
 	return p;
 }
 
-void print_f(void* a) {
-	printf("%d", *((int*)a));
+/**
+ * create_stack_with_elements() - Creates a empty stack and fills it with elements.
+ * @values: An array of values to add to the stack.
+ * @lenght: How long the array of values is.
+ * 
+ * Returns: A stack pointer
+*/
+stack* create_stack_with_elements(int *values, int length) {
+	stack *s = stack_empty(free);
+
+	for (int i = 0; i < length; i++)
+	{
+		s = stack_push(s, create_element(values[i]));
+	}
+	
+	return s;
 }
 
 /**
@@ -100,12 +118,9 @@ void empty_is_empty() {
 void one_element_is_non_empty() {
 	fprintf(stderr, "Starting one_element_is_non_empty()...");
 
-	// Creating an empty stack
-	stack *s = stack_empty(free);
-
-	// Create dynamic element, pushes it to the stack
-	int *val = create_element(42);
-	s = stack_push(s, val);
+	// Create stack with element from value
+	int value[] = {42};
+	stack *s = create_stack_with_elements(value, 1);
 
 	if (stack_is_empty(s))
 	{
@@ -122,6 +137,99 @@ void one_element_is_non_empty() {
 }
 
 /**
+ * element_add_remove_empty() - Tests if pop() does nothing.
+ * 
+ * Returns: Nothing
+*/
+void element_add_remove_empty() {
+	fprintf(stderr, "Started element_add_remove_empty()...");
+
+	// Create stack with value from value
+	int value[] = {42};
+	stack *s = create_stack_with_elements(value, 1);
+	
+	// Removes value from stack
+	s = stack_pop(s);
+
+	if (!stack_is_empty(s))
+	{
+		// Fails with error message
+		fprintf(stderr, "FAIL: Expected is_empty() == true, got false.\n");
+		stack_kill(s);
+		exit(EXIT_FAILURE);
+	}
+	
+	// Everything went as expected, clean up
+	fprintf(stderr, "Cleaning up...");
+	stack_kill(s);
+	fprintf(stderr, "DONE!\n");	
+}
+
+/**
+ * inserted_too_many_values() - Test if stack_push adds to many values to the stack.
+ * 
+ * Returns: Nothing
+*/
+void inserted_too_many_values() {
+	fprintf(stderr, "Starting inserted_too_many_values()...");
+	
+	// Create stack with element from value
+	int value[] = {42, 24};
+	stack *s = create_stack_with_elements(value, 2);
+
+	stack_print(s, print_f);
+	
+	//Removes the two added elements
+	s = stack_pop(stack_pop(s));
+
+	stack_print(s, print_f);
+
+	// stack_is_empty() should return true
+	if (!stack_is_empty(s))
+	{
+		// Fail with error message
+		fprintf(stderr, "FAIL: Expected stack_is_empty() == true, got false. Too many elements added.\n");
+		stack_kill(s);
+		exit(EXIT_FAILURE);
+	}
+	
+	// Everything went as planned, clean up.
+	fprintf(stderr, "Cleaning up...");
+	stack_kill(s);
+	fprintf(stderr, "DONE!\n");
+}
+
+/**
+ * removed_correct_element() - Tests if pop removes the correct element.
+ * 
+ * Returns: Nothing.
+*/
+void removed_correct_element() {
+		fprintf(stderr, "Started removed_correct_element()...");
+
+	// Create stack with value from value
+	int value[] = {42, 24};
+	stack *s = create_stack_with_elements(value, 2);
+	
+	// Removes value from stack and reads the top
+	s = stack_pop(s);
+	int read_value = *((int*)stack_top(s));
+
+	if (read_value != value[0])
+	{
+		// Fails with error message
+		fprintf(stderr, "FAIL: Expected top() == %d, got %d.\n", value[0], read_value);
+		stack_kill(s);
+		exit(EXIT_FAILURE);
+	}
+	
+	// Everything went as expected, clean up
+	fprintf(stderr, "Cleaning up...");
+	stack_kill(s);
+	fprintf(stderr, "DONE!\n");	
+}
+
+/**
  * inserted_element_has_correct_value() - Tests that pushed element has correct value.
  * 
  * Returns: Nothing.
@@ -129,19 +237,18 @@ void one_element_is_non_empty() {
 void inserted_element_has_correct_value() {
 	fprintf(stderr, "Starting inserted_element_has_correct_value()...");
 
-	// Create empty stack, adds value to stack
-	stack *s = stack_empty(free);
-	int *val = create_element(42);
-	s = stack_push(s, val);
+	// Create stack with element from value
+	int value[] = {42};
+	stack *s = create_stack_with_elements(value, 1);
 
 	// Read top element
 	int *read_val = stack_top(s);
 
 	// Expecting read_val == val
-	if (*read_val != *val)
+	if (*read_val != value[0])
 	{
 		// Fail with error message
-		fprintf(stderr, "FAIL: Expected top() == %d, got %d.\n", *val, *read_val);
+		fprintf(stderr, "FAIL: Expected top() == %d, got %d.\n", value[0], *read_val);
 		stack_kill(s);
 		exit(EXIT_FAILURE);
 	}
@@ -160,17 +267,17 @@ void inserted_element_has_correct_value() {
 void read_top_test() {
 	fprintf(stderr, "Starting read_top_test()...");
 
-	// Create empty stack, values and pushes them to the stack
-	stack *s = stack_empty(free);
-	int *val1 = create_element(42);
-	int *val2 = create_element(24);
-	s = stack_push(s, val1);
-	s = stack_push(s, val2);
+	// Create stack with element from values
+	int values[] = {42, 24};
+	stack *s = create_stack_with_elements(values, 2);
 
-	if (stack_top(s) != val2)
+	// Read top
+	int read_val = *((int*)stack_top(s));
+
+	if (read_val != values[1])
 	{
 		// Fails with error message
-		fprintf(stderr, "FAIL: Expected top() == %d, got %d.\n", *val2, *(int*)stack_top(s));
+		fprintf(stderr, "FAIL: Expected top() == %d, got %d.\n", values[2], read_val);
 	}
 	
 	// Everything went as expected, clean up
@@ -181,24 +288,23 @@ void read_top_test() {
 
 /**
  * top_is_constant() - Tests if top returns constant values
+ * 
+ * Returns: Nothing.
 */
 void top_is_constant() {
 	fprintf(stderr, "Starting top_is_constant()...");
 
-	// Create empty stack, values and pushes them to the stack
-	stack *s = stack_empty(free);
-	int *values[] = {create_element(42), create_element(24)};
-	int *read_values[2];
+	// Create stack with element from values
+	int values[] = {42, 24, 64};
+	stack *s = create_stack_with_elements(values, 3);
 
-	// Pushes the values to the stack and reads top().
-	for (int i = 0; i < 2; i++)
-	{
-		s = stack_push(s, values[i]);
-		read_values[i] = stack_top(s);
-	}
+	//Read values from stack
+	int read_val1 = *(int*)stack_top(s);
+	s = stack_pop(s);
+	int read_val2 = *(int*)stack_top(s);
 
 	// value_equal should be false here
-	if (value_equal(read_values[0], read_values[1]))
+	if (value_equal(&read_val1, &read_val2))
 	{
 		// Fails with error message
 		fprintf(stderr, "FAIL: top() returns constant value.\n");
@@ -221,32 +327,23 @@ void top_is_constant() {
 void inserted_elements_at_correct_place() {
 	fprintf(stderr, "Starting inserted_elements_at_correct_place()...");
 
-	// Creates empty stack and an array of values to push to the stack
+	// Create stack with element from values
 	int n = 5;
-	int *values[] = {
-		create_element(321), 
-		create_element(21), 
-		create_element(42), 
-		create_element(6), 
-		create_element(13)
-	};
-	stack *s = stack_empty(free);
-
-	// Pushes values to stack
-	for (int i = 0; i < n; i++)
-	{
-		s = stack_push(s, values[i]);
-	}
+	int values[] = {321, 21, 42, 6, 13};
+	stack *s = create_stack_with_elements(values, n);
 	
 	// Loops through the stack
 	for (int i = 0; i < n; i++)
 	{
+		// Read top
+		int *read_val = ((int*)stack_top(s));
+
 		// read values from stack and compares them
-		if (!value_equal(values[(n - 1) - i], (int*)stack_top(s)))
+		if (!value_equal(&values[(n - 1) - i], read_val))
 		{
 			// Fail with error message
 			fprintf(stderr, "FAIL: Expected top() element nr.%d to be %d, got %d.\n", i+1, 
-				*(values[(n - 1) - i]), *(int*)stack_top(s));
+				values[(n - 1) - i], *read_val);
 			stack_kill(s);
 			exit(EXIT_FAILURE);
 		}
@@ -262,35 +359,6 @@ void inserted_elements_at_correct_place() {
 }
 
 /**
- * inserted_too_many_values() - Test if stack_push adds to many values to the stack.
- * 		Also catches if pop does not remove the last/only element in the stack.
- * 
- * Returns: nothing
-*/
-void inserted_too_many_values() {
-	fprintf(stderr, "Starting inserted_too_many_values()...");
-	
-	// Creates empty stack and pushes a value to it and removes that value
-	int *val = create_element(42);
-	stack *s = stack_empty(free);
-	s = stack_pop(stack_push(s, val));
-
-	// stack_is_empty() should return true
-	if (!stack_is_empty(s))
-	{
-		// Fail with error message
-		fprintf(stderr, "FAIL: Expected stack_is_empty() == true, got false. Too many elements added.\n");
-		stack_kill(s);
-		exit(EXIT_FAILURE);
-	}
-	
-	// Everything went as planned, clean up.
-	fprintf(stderr, "Cleaning up...");
-	stack_kill(s);
-	fprintf(stderr, "DONE!\n");
-}
-
-/**
  * main() - Runs all the tests.
 */
 int main(void)
@@ -298,11 +366,13 @@ int main(void)
 	empty_returns_non_null();
 	empty_is_empty();
 	one_element_is_non_empty();
+	element_add_remove_empty();
+	inserted_too_many_values();
+	removed_correct_element();
 	inserted_element_has_correct_value();
 	read_top_test();
 	top_is_constant();
 	inserted_elements_at_correct_place();
-	inserted_too_many_values();
 
 	return 0;
 }
